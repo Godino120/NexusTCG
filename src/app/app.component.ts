4ada7bf16f878a9d51ca53db55220984e401a8c1
Godino120 <jgodinorodas@gmail.com>
2024-12-12 14:48:38 +0100

    Contactos nuevo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from "./nav-bar/nav-bar.component";
 import { FooterComponent } from "./footer/footer.component";
+import { ContactoComponent } from "./contacto/contacto.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent, FooterComponent],
+  imports: [RouterOutlet, NavBarComponent, FooterComponent, ContactoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
