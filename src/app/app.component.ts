76027d9a45b0861e9a37af0e89706a0bedc8514c
Godino120 <jgodinorodas@gmail.com>
2024-12-13 14:30:54 +0100

    Rutas Inicio y Contacto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from "./nav-bar/nav-bar.component";
 import { FooterComponent } from "./footer/footer.component";
 import { ContactoComponent } from "./contacto/contacto.component";
+import { InicioComponent } from "./inicio/inicio.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent, FooterComponent, ContactoComponent],
+  imports: [RouterOutlet, NavBarComponent, FooterComponent, ContactoComponent, InicioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ContactoComponent } from './contacto/contacto.component';
+import { InicioComponent } from './inicio/inicio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "contacto", component: ContactoComponent},
+    {path: "inicio", component: InicioComponent}
+];
