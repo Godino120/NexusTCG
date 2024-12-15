82ac59d49d8daf913444195517074ec0b1cbcb16
Godino120 <jgodinorodas@gmail.com>
2024-12-15 15:19:31 +0100

    Ruta predeterminada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { ContactoComponent } from './contacto/contacto.component';
 import { InicioComponent } from './inicio/inicio.component';
 
 export const routes: Routes = [
+    { path: "", redirectTo: "inicio", pathMatch: "full" },
     {path: "contacto", component: ContactoComponent},
     {path: "inicio", component: InicioComponent}
 ];
