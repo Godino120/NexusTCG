f1389805d4fbc80bf0180b4793a5a32a235227d5
Godino120 <jgodinorodas@gmail.com>
2025-02-04 14:45:52 +0100

    Registro e Inicio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { ContactoComponent } from './contacto/contacto.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { RegistroComponent } from './registro/registro.component';
+import { SesionComponent } from './sesion/sesion.component';
 
 export const routes: Routes = [
     { path: "", redirectTo: "inicio", pathMatch: "full" }, 
     { path: "contacto", component: ContactoComponent },
     { path: "inicio", component: InicioComponent },
-    { path: "registro", component: RegistroComponent }
+    { path: "registro", component: RegistroComponent },
+    { path: "iniciarSesion", component: SesionComponent}
 ];
---
+++ src/app/sesion/sesion.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-sesion',
+  standalone: true,
+  imports: [RouterModule, FormsModule],
+  templateUrl: './sesion.component.html',
+  styleUrl: './sesion.component.css'
+})
+export class SesionComponent {
+  constructor(private services: AuthService, private ruta: Router){}
+
+  email!:string;
+  password!:string;
+
+  async login(){
+    await this.services.login(this.email, this.password);
+    this.ruta.navigate(['/']);
+  }
+}
